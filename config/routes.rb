f5e135b51dfb984cb591fde1da5a6fc25ef67aae
PierminatorQ <pierrequintero@gmail.com>
2021-01-16 14:06:51 -0300

    namespaces routes Changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    namespace :v1 do
+  namespace :api, defaults: { format: :json }, path: '/api' do
+    namespace :v1 , defaults: { format: :json }, path: '/v1' do
       resources :quizzes, param: :id
       resources :questions, param: :quiz_id
     end
